b443863a85f265d120e55fbeb4d425297783cf89
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-03-17 01:23:39 +0700

    add Task_1Activity.kt

--- app/src/main/java/ru/zdanovich/handhSchoolHomework/MainActivity.kt
+++ app/src/main/java/ru/zdanovich/handhSchoolHomework/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.zdanovich.handhSchoolHomework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.ma_button_task_1).setOnClickListener {
+            val intent = Intent(this, Task_1Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
