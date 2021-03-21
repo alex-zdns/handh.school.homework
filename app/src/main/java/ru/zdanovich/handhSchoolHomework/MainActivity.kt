f67de78a9c2c40000826cfb5cee795605a129266
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-03-21 22:38:16 +0700

    rename module from lesson_1 to app

--- app/src/main/java/ru/zdanovich/handhSchoolHomework/MainActivity.kt
+++ app/src/main/java/ru/zdanovich/handhSchoolHomework/MainActivity.kt
@@ -1,9 +1,9 @@
 package ru.zdanovich.handhSchoolHomework
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import ru.zdanovich.handhSchoolHomework.task2.TaskTwoActivity
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/ru/zdanovich/handhSchoolHomework/TaskOneActivity.kt
+++ app/src/main/java/ru/zdanovich/handhSchoolHomework/TaskOneActivity.kt
@@ -1,11 +1,11 @@
 package ru.zdanovich.handhSchoolHomework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class TaskOneActivity : AppCompatActivity() {
