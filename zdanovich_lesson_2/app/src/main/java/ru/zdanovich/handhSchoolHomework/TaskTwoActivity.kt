ed50bc5425e19385c66ec656820a2c4df67b3594
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-03-27 02:13:00 +0700

    zdanovich_lesson_2 (#4)
    * add lesson_2
    
    * add sizes in dimens
    
    * add sizes in dimens.xml
    
    * add colors in colors.xml

---
+++ zdanovich_lesson_2/app/src/main/java/ru/zdanovich/handhSchoolHomework/TaskTwoActivity.kt
@@ -0,0 +1,11 @@
+package ru.zdanovich.handhSchoolHomework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_two)
+    }
+}
