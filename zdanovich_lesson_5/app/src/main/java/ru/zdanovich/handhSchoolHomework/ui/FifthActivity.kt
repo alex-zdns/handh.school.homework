376a44b192834d10efd888dc4e600780ea0d5790
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-04-03 01:23:59 +0700

    add task 4

--- zdanovich_lesson_5/app/src/main/java/ru/zdanovich/handhSchoolHomework/ui/FifthActivity.kt
+++ zdanovich_lesson_5/app/src/main/java/ru/zdanovich/handhSchoolHomework/ui/FifthActivity.kt
@@ -4,20 +4,36 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.zdanovich.handhSchoolHomework.databinding.ActivityFifthBinding
+import ru.zdanovich.handhSchoolHomework.domain.models.Data
 
 
 class FifthActivity : AppCompatActivity() {
     private var _binding: ActivityFifthBinding? = null
     private val binding get() = _binding!!
 
+    private var data: Data? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityFifthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (savedInstanceState != null) {
+            data = savedInstanceState.getParcelable(DATA) as Data?
+            data?.let {
+                binding.activityFifthTextView.text = data?.value
+            }
+        }
+
         binding.activityFifthDeliverResultButton.setOnClickListener {
             val message = binding.activityFifthEditText.text.toString()
             sendMessage(message)
+        }
+
+        binding.activityFifthSaveButton.setOnClickListener {
+            val text = binding.activityFifthEditText.text.toString()
+            data = Data(text)
+            binding.activityFifthTextView.text = text
         }
     }
 
@@ -28,8 +44,20 @@ class FifthActivity : AppCompatActivity() {
         finish()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        data?.let {
+            outState.putParcelable(DATA, it)
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        const val DATA = "DATA"
     }
 }
