a5c944e8743017a0b600b01dff2cff86ec02387b
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-04-11 21:46:53 +0700

    change application icon

--- zdanovich_lesson_7/app/src/main/java/ru/zdanovich/handhSchoolHomework/presenter/helpers/BridgeIcon.kt
+++ zdanovich_lesson_7/app/src/main/java/ru/zdanovich/handhSchoolHomework/presenter/helpers/BridgeIcon.kt
@@ -4,7 +4,7 @@ import ru.zdanovich.handhSchoolHomework.R
 import ru.zdanovich.handhSchoolHomework.domain.models.Bridge
 
 fun getBridgeIcon(status: Bridge.BridgeStatus): Int = when (status) {
-    Bridge.BridgeStatus.Close -> R.drawable.ic_brige_late
-    Bridge.BridgeStatus.Open -> R.drawable.ic_brige_normal
-    Bridge.BridgeStatus.SoonClose -> R.drawable.ic_brige_soon
+    Bridge.BridgeStatus.Close -> R.drawable.ic_bridge_late
+    Bridge.BridgeStatus.Open -> R.drawable.ic_bridge_normal
+    Bridge.BridgeStatus.SoonClose -> R.drawable.ic_bridge_soon
 }
