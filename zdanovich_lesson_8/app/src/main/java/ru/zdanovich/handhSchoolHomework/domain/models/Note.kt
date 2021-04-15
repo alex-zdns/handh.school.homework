427aa9a4ff60bee47deb591a4c95ad6d0d9bad86
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-04-15 22:08:38 +0700

    add Parcelable for domain Note

--- zdanovich_lesson_8/app/src/main/java/ru/zdanovich/handhSchoolHomework/domain/models/Note.kt
+++ zdanovich_lesson_8/app/src/main/java/ru/zdanovich/handhSchoolHomework/domain/models/Note.kt
@@ -1,7 +1,11 @@
 package ru.zdanovich.handhSchoolHomework.domain.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Note(
     val id: Int = -1,
     val title: String,
     val body: String
-)
+) : Parcelable
