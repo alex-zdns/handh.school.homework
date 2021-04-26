cc70ba6320894ad7d11094992ea379b720c35a27
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-04-26 19:42:34 +0700

    refactor BridgesMapFragment.kt

--- zdanovich_lesson_10/app/src/main/java/ru/zdanovich/handhSchoolHomework/presenter/bridgeMap/BridgesMapFragment.kt
+++ zdanovich_lesson_10/app/src/main/java/ru/zdanovich/handhSchoolHomework/presenter/bridgeMap/BridgesMapFragment.kt
@@ -1,39 +1,41 @@
 package ru.zdanovich.handhSchoolHomework.presenter.bridgeMap
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.snackbar.Snackbar.SnackbarLayout
 import ru.zdanovich.handhSchoolHomework.R
 import ru.zdanovich.handhSchoolHomework.databinding.FragmentBridgesMapBinding
 import ru.zdanovich.handhSchoolHomework.databinding.ViewHolderBridgeBinding
-import ru.zdanovich.handhSchoolHomework.presenter.bridgeList.BridgeAdapter
-import ru.zdanovich.handhSchoolHomework.presenter.bridgeList.BridgeListState
-import ru.zdanovich.handhSchoolHomework.presenter.bridgeList.BridgesListViewModel
-import ru.zdanovich.handhSchoolHomework.presenter.bridgeList.BridgesListViewModelFactory
-import ru.zdanovich.handhSchoolHomework.presenter.helpers.getBridgeIcon
-import ru.zdanovich.handhSchoolHomework.presenter.helpers.getBridgeIconBitmap
+import ru.zdanovich.handhSchoolHomework.domain.models.Bridge
+import ru.zdanovich.handhSchoolHomework.presenter.bridgeList.*
+import ru.zdanovich.handhSchoolHomework.presenter.helpers.onBind
 
 class BridgesMapFragment : androidx.fragment.app.Fragment(), OnMapReadyCallback,
     GoogleMap.OnMarkerClickListener {
     private var _binding: FragmentBridgesMapBinding? = null
     private val binding get() = _binding!!
-
+    private var listenerBridgesList: BridgesListFragment.BridgesListClickListener? = null
     private val viewModel: BridgesListViewModel by viewModels { BridgesListViewModelFactory() }
+    private var mMap: GoogleMap? = null
+    private lateinit var bridgesMap: Map<Int, Bridge>
 
-    private lateinit var mMap: GoogleMap
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listenerBridgesList = context as? BridgesListFragment.BridgesListClickListener
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,68 +48,157 @@ class BridgesMapFragment : androidx.fragment.app.Fragment(), OnMapReadyCallback,
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        //viewModel.state.observe(this.viewLifecycleOwner, this::setState)
+        setupToolbar()
 
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.fbm_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        viewModel.state.observe(this.viewLifecycleOwner, this::setState)
         if (viewModel.state.value is BridgeListState.Default) viewModel.getBridges()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun setupToolbar() {
+        binding.fbmToolbar.apply {
+            inflateMenu(R.menu.fragment_bridges_map_menu)
+
+            setNavigationOnClickListener {
+                activity?.onBackPressed()
+            }
+
+            setOnMenuItemClickListener {
+                if (it.itemId == R.id.action_fbm_refresh) {
+                    refresh()
+                    return@setOnMenuItemClickListener true
+                }
+
+                return@setOnMenuItemClickListener false
+            }
+        }
+    }
+
+    private fun refresh() {
+        viewModel.getBridges()
+    }
+
+    private fun setState(bridgeListState: BridgeListState) =
+        when (bridgeListState) {
+            is BridgeListState.Default,
+            is BridgeListState.Loading -> {
+                setLoading(true)
+            }
+            is BridgeListState.Error.Internet -> {
+                setLoading(false)
+                showToast(getString(R.string.error_internet_message))
+            }
+            is BridgeListState.Error.Other -> {
+                setLoading(false)
+                showToast(getString(R.string.other_error_message))
+            }
+            is BridgeListState.Success -> {
+                mapBridgesAndSetOnMap(bridgeListState.bridges)
+                setLoading(false)
+            }
+        }
+
+    private fun mapBridgesAndSetOnMap(bridges: List<Bridge>) {
+        bridgesMap = bridges.associateBy { it.id }
+        setBridgesOnMap()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun setLoading(loading: Boolean) {
+        binding.fbmLoader.isVisible = loading
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        if (viewModel.state.value is BridgeListState.Success) {
-            val bridgeList = (viewModel.state.value as BridgeListState.Success).bridges
-
-            for (bridge in bridgeList) {
-                val bridgeCoordinate =
-                    LatLng(bridge.coordinate.latitude, bridge.coordinate.longitude)
-                val marker = mMap.addMarker(
-                    MarkerOptions()
-                        .position(bridgeCoordinate)
-                        .icon(BitmapDescriptorFactory.fromResource(getBridgeIconBitmap(bridge.getBridgeStatus())))
-                )
-
-                marker.tag = bridge.id
-            }
-
-            mMap.setOnMarkerClickListener(this)
+        val state = viewModel.state.value
+        if (state is BridgeListState.Success) {
+            mapBridgesAndSetOnMap(state.bridges)
         }
 
-        val spbCenter = LatLng(59.57, 30.19)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(spbCenter))
+        googleMap.setOnMarkerClickListener(this)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(spbRegion, 0))
+    }
+
+    private fun setBridgesOnMap() {
+        for (bridge in bridgesMap.values) {
+            val bridgeCoordinate =
+                LatLng(bridge.coordinate.latitude, bridge.coordinate.longitude)
+            val marker = mMap?.addMarker(
+                MarkerOptions()
+                    .position(bridgeCoordinate)
+                    .icon(getBridgeIconBitmap(bridge.getBridgeStatus()))
+            )
+
+            marker?.tag = bridge.id
+        }
     }
 
     override fun onMarkerClick(marker: Marker): Boolean {
         val id: Int = marker.tag as Int
-        if (viewModel.state.value is BridgeListState.Success) {
-            val bridgeList = (viewModel.state.value as BridgeListState.Success).bridges
-            val bridge = bridgeList[id - 1]
-            val snackbar = Snackbar.make(binding.root, "", Snackbar.LENGTH_LONG)
-
+        bridgesMap[id]?.let { bridge ->
+            val snackBar = Snackbar.make(binding.root, EMPTY, Snackbar.LENGTH_LONG)
             val snackBinding = ViewHolderBridgeBinding.inflate(layoutInflater)
-            snackBinding.vhbStatusIcon.setImageResource(getBridgeIcon(bridge.getBridgeStatus()))
-            snackBinding.vhbBridgeTimeDivorces.text =
-                bridge.bridgeDivorcesTimes.joinToString(postfix = BridgeAdapter.BRIDGE_DIVORCES_TIME_POSTFIX) { it.toUiString() }
-            snackBinding.vhbBridgeName.text = bridge.name
+            snackBinding.onBind(bridge)
+            val snackBarLayout = snackBar.view as SnackbarLayout
+            snackBar.view.setBackgroundColor(Color.WHITE)
 
-            val snackbarLayout = snackbar.view as SnackbarLayout
-            snackbar.view.setBackgroundColor(Color.WHITE)
-            snackbarLayout.setPadding(0, 0, 0, 0)
-            snackbarLayout.addView(snackBinding.root)
-            snackbar.animationMode = Snackbar.ANIMATION_MODE_SLIDE
+            snackBar.view.setOnClickListener {
+                snackBar.dismiss()
+                listenerBridgesList?.openBridgeInfoFragment(bridge)
+            }
 
-
-            snackbar.show()
+            snackBarLayout.addView(snackBinding.root)
+            snackBar.animationMode = Snackbar.ANIMATION_MODE_SLIDE
+            snackBar.show()
         }
+
         return true
+    }
+
+    private fun getBridgeIconBitmap(status: Bridge.BridgeStatus): BitmapDescriptor =
+        when (status) {
+            Bridge.BridgeStatus.Close -> bridgeBitmapCloseMarker
+            Bridge.BridgeStatus.Open -> bridgeBitmapOpenMarker
+            Bridge.BridgeStatus.SoonClose -> bridgeBitmapSoonMarker
+        }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mMap?.setOnMapClickListener(null)
+        _binding = null
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listenerBridgesList = null
+    }
+
+    companion object {
+        const val EMPTY = ""
+        val spbRegion by lazy { LatLngBounds(LatLng(59.51, 29.58), LatLng(60.18, 30.97)) }
+        val bridgeBitmapCloseMarker: BitmapDescriptor by lazy {
+            BitmapDescriptorFactory.fromResource(
+                R.drawable.ic_brige_late
+            )
+        }
+
+        val bridgeBitmapSoonMarker: BitmapDescriptor by lazy {
+            BitmapDescriptorFactory.fromResource(
+                R.drawable.ic_brige_soon
+            )
+        }
+
+        val bridgeBitmapOpenMarker: BitmapDescriptor by lazy {
+            BitmapDescriptorFactory.fromResource(
+                R.drawable.ic_brige_normal
+            )
+        }
     }
 }
